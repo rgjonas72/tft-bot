b6988fbd054683185d4d8d5c630bf389e4898989
rgjonas72 <rgjonas72@gmail.com>
2025-07-08 22:01:06 -0500

    Update tft_bot.py

--- tft_bot.py
+++ tft_bot.py
@@ -295,7 +295,7 @@ async def ended_games_loop():
             puuid = active_game[1]
             units, placement, full_pic = tft_stuff.get_user_unit_info(puuid, tft_game)
             sql_stuff.update_game_on_finish(puuid, game_id, placement, units)
-            #await message_user_game_ended(disc_id, game_id, full_pic)
+            await message_user_game_ended(disc_id, game_id, full_pic)
         await asyncio.sleep(3)
 
 
