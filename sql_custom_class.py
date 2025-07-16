264c1beae356c7fb50741afa102dd0d86bc4e929
rgjonas72 <rgjonas72@gmail.com>
2025-07-16 12:59:22 -0500

    Update sql_custom_class.py

--- sql_custom_class.py
+++ sql_custom_class.py
@@ -171,7 +171,7 @@ class sql_stuff_class():
     def get_augment_stats(self, augment):
         self.cnx.reconnect()
         with self.cnx.cursor() as cursor:
-            cursor.execute("SELECT avg(placement) FROM games WHERE aug1=%s OR aug2=%s OR aug3=%s OR aug4=%s", (*[augment]*4,))
+            cursor.execute("SELECT round(avg(placement), 1) FROM games WHERE aug1=%s OR aug2=%s OR aug3=%s OR aug4=%s", (*[augment]*4,))
             avp = cursor.fetchone()[0]
         print(avp)
         return avp
